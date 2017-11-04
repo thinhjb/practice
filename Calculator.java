3ef7f08b7853d576db38b33a0cf2db9366d77128
Thinh Nguyen <thinhjb@lnt.website>
2017-11-04 11:52:30 -0700

    add subtract
    test
    
    Signed-off-by: Thinh Nguyen <thinhjb@lnt.website>

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator
 {
+
+    public int subtract(int first, int second) {
+        return first - second;
+    }
+
 	public static void main(String[] args) {
 		System.out.println("Dante Do");
 	}
