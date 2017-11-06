6d5a46cf8a0a1a2a2cb991d3cd4eecc2a89c17c2
Luong <Luong@DESKTOP-V88BA16>
2017-11-05 22:58:41 -0800

    Calculator
    full function
    
    
    Signed-off-by: Luong <Luong@DESKTOP-V88BA16>

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,30 @@
-
-
+import java.util.Scanner;
 
 public class Calculator
 {
 
-    public int subtract(int first, int second) {
+    public static int subtract(int first, int second) {
         return first - second;
     }
 
-    public int add(int first, int second) {
+    public static int add(int first, int second) {
         return first + second;
+    }
+    
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
+    
+    public static int divide(int first, int second) {
+        return first / second;
+    }
+    
+    public static int exponential(int first, int second)
+    {
+    	for (int x= second; x>1; x--)
+    	{first= first*first;}
+    	return first;
+    	
     }
 
     public void test() {
@@ -17,6 +32,36 @@ public class Calculator
     }    
 
 	public static void main(String[] args) {
-		System.out.println("Dante Do");
+	    int answer=0;
+	    int error= 0;
+		Scanner scanner = new Scanner(System.in);
+		for( ; ;)
+		{
+		error =0;	
+		System.out.println("Enter the first number:");
+		int firstnumber = scanner.nextInt(); 
+		System.out.println("Enter the application: + - * / ^");
+		String application = scanner.next();
+		System.out.println("Enter the second number");
+		int secondnumber = scanner.nextInt();
+	
+		if (application.equals("+"))
+		answer = Calculator.add(firstnumber, secondnumber);
+		else if (application.equals("-"))
+		answer = Calculator.subtract(firstnumber, secondnumber);
+		else if (application.equals("*"))
+		answer = Calculator.multiply(firstnumber, secondnumber);
+		else if (application.equals("/"))
+		answer = Calculator.divide(firstnumber, secondnumber);
+		else if (application.equals("^"))
+		answer = Calculator.exponential(firstnumber, secondnumber);
+		else 
+		{System.out.println("The application is incorrect please try again");
+		error = 1;}
+		if (error == 0)
+			System.out.println("The answer is " + answer);
+	
+		}
 	}
 }
+
