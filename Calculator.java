ea894a194ba42fa9245c460d31e56f5e385d85df
Thinh Nguyen <thinhjb@gmail.com>
2017-11-18 14:26:02 -0500

    Reorganize code according to K&R
    Fix indentation and variable naming to help with reading. Follow K&R
    style and keep consistent format.
    
    Signed-off-by: Thinh Nguyen <thinhjb@gmail.com>

--- Calculator.java
+++ Calculator.java
@@ -1,52 +1,51 @@
 import java.util.Scanner;
 
+public class Calculator {
+	public int subtract(int first, int second) {
+		return 0;
+	}
 
-public class Calculator
-{
+	public int add(int first, int second) {
+		return 0;
+	}
 
-    public int subtract(int first, int second) {
-        return first - second;
-    }
-
-    public int add(int first, int second) {
-        return first + second;
-    }
-
-    public void test() {
-        System.out.println("test");
-    }
-    
-}
+	public void test() {
+		System.out.println("test");
+	}
 
 	public static void main(String[] args) {
-	    int answer=0;
-	    int error= 0;
-		Scanner scanner = new Scanner(System.in);
-		for( ; ;)
-		{
-		error =0;	
-		System.out.println("Enter the first number:");
-		int firstnumber = scanner.nextInt(); 
-		System.out.println("Enter the application: + - * / ^");
-		String application = scanner.next();
-		System.out.println("Enter the second number");
-		int secondnumber = scanner.nextInt();
-	
-		if (application.equals("+"))
-		answer = Calculator.add(firstnumber, secondnumber);
-		else if (application.equals("-"))
-		answer = Calculator.subtract(firstnumber, secondnumber);
-		else if (application.equals("*"))
-		answer = Calculator.mult(firstnumber, secondnumber);
-		else if (application.equals("/"))
-		answer = Calculator.division(firstnumber, secondnumber);
-		else if (application.equals("^"))
-		answer = Calculator.power(firstnumber, secondnumber);
-		else 
-		{System.out.println("The application is incorrect please try again");
-		error = 1;}
-		if (error == 0)
-			System.out.println("The answer is " + answer);
-	
+	    int answer = 0;
+	    int error = 0;
+	    int first = 0;
+	    int second = 0;
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+
+		while (true) {
+			error = 0;
+			System.out.println("Enter the first number:");
+			first = scanner.nextInt();
+			System.out.println("Enter the application: + - * / ^");
+			String application = scanner.next();
+			System.out.println("Enter the second number");
+			second = scanner.nextInt();
+
+			if (application.equals("+")) {
+				answer = Calculator.add(first, second);
+			} else if (application.equals("-")) {
+				answer = Calculator.subtract(first, second);
+			} else if (application.equals("*")) {
+				answer = Calculator.mult(first, second);
+			} else if (application.equals("/")) {
+				answer = Calculator.division(first, second);
+			} else if (application.equals("^")) {
+				answer = Calculator.power(first, second);
+			} else {
+				System.out.println("The application is incorrect please try again");
+				error = 1;
+			}
+			if (error == 0)
+				System.out.println("The answer is " + answer);
 		}
 	}
+};
